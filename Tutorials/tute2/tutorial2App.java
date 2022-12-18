7731253764e57dd7cfcdb7cc3f180a2f175fa8c7
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2022-12-18 17:52:55 +0530

    edited

---
+++ Tutorials/tute2/tutorial2App.java
@@ -0,0 +1,12 @@
+package Tutorials.tute2;
+
+public class tutorial2App {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        calcAvg avg = new calcAvg();
+        avg.calcAvg(50, 60, 70);
+    }
+    
+}
