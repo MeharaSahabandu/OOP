0000aa4c09aab3bb7123cc69b779558ab909a58c
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-28 10:21:54 +0530

    repetition using 'foor'

---
+++ Lectures/Lecture_1/useFor.java
@@ -0,0 +1,12 @@
+package Lectures.Lecture_1;
+
+public class useFor {
+
+    public static void main (String[] args){
+        int num;
+        for(num=1;num<=5;num++){
+            System.out.println(num);
+        }
+    }
+    
+}
